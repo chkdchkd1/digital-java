c568edc87cfcb02737fd3389b8c12d721521ed3e
kjs <chkdchkd1@naver.com>
2020-04-21 12:19:38 +0900

    day3 break, continue 예제

---
+++ src/day3/ForEx8.java
@@ -0,0 +1,25 @@
+package day3;
+
+public class ForEx8 {
+
+	public static void main(String[] args) {
+		// break, continue 예제
+		for(int i=1;i<=5;i++) {System.out.println("Hello");
+		if(i==3) {
+			break;			
+		}
+
+		}
+		
+	
+		for(int i = 1; i<=10; i++) {
+			if(i % 2 == 1) {
+				continue;
+			}
+			System.out.println(i);
+		}
+		for ( ; ; ) {
+			
+		}
+}
+}
