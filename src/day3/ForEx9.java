316c0e32c4ddf00db1f3ed79bfbd021da7e1deea
kjs <chkdchkd1@naver.com>
2020-04-21 14:07:03 +0900

    중첩 for문 * 별무늬 찍기

--- src/day3/ForEx9.java
+++ src/day3/ForEx9.java
@@ -3,7 +3,7 @@ package day3;
 public class ForEx9 {
 
 	public static void main(String[] args) {
-		// 중첩 for문 
+		// 중첩 for문 * 별무늬 찍기
 		
 		// 그냥 for문을 이용해서 
 		/*for(int j=1; j<=5; j++) {
@@ -17,6 +17,33 @@ public class ForEx9 {
 			}
 			System.out.println();
 		}
+		/* *		i=1	*=1
+		 * **		i=2 *=2
+		 * *** 		i=3 *=3
+		 * **** 	i=4 *=4
+		 * *****	i=5 *=5
+		 * 			*=1
+		 * 공백= 5-i     */
+		
+		for(int i = 1; i<=5; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		
+		for(int i = 1; i<=5; i++) {
+			for(int j=1; j<=5-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			
+		
 	}
 
+}
 }
