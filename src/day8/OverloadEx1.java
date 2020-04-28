c92ebd9718c73995cde570d148978c633ff1cc1d
kjs <chkdchkd1@naver.com>
2020-04-28 17:45:06 +0900

    day8 오버로딩 예제 및 가변인자 예제

---
+++ src/day8/OverloadEx1.java
@@ -0,0 +1,33 @@
+package day8;
+
+public class OverloadEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//println의 메서드 오버로딩을 확인을 위한 예제 
+		System.out.println(5); //public void println(int x)
+		System.out.println('5'); //public void println(char x)
+		System.out.println("5"); //public void println(String x)
+		System.out.println(5.0f); //public void println(float x)
+		System.out.println(5.0); //public void println(double x)
+		System.out.println(false); //public void  println(boolean x)
+		System.out.println(5L); //public void println(long x)
+		
+		printString();
+		printString("홍길동");
+		printString("홍길동","임꺽정");
+		printString("홍길동","임꺽정","이순신");
+
+	}
+	
+	// 밑 str는 배열 , 가변인자는 배열을 이용한다.
+	// 매개변수를 가변인자로 하는 매서드 예제 
+	public static void printString(String...str) {
+		for(String tmp : str) {
+			System.out.print(tmp+" ");
+		}
+		System.out.println();
+	}
+
+}
