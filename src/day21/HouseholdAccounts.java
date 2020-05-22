634d0ea9149f541d82a2d12843e8cdb417993981
kjs <chkdchkd1@naver.com>
2020-05-22 17:48:02 +0900

    day21 가계부 프로그램 (self, 완)

--- src/day21/HouseholdAccounts.java
+++ src/day21/HouseholdAccounts.java
@@ -2,6 +2,8 @@ package day21;
 
 import java.util.*;
 
+
+
 public class HouseholdAccounts {
 
 	public static void main(String[] args) {
@@ -14,31 +16,117 @@ public class HouseholdAccounts {
 			   - 등록된 리스트 중에 수정할 내용을 선택 => 선택한 내용을 수정
 			 3. 확인
 			   - 일시를 기준으로 확인
-			   - 수입/지출을 기준으로 확인
+			   - 수입/지출을 기준으로 확인  => 이걸 날짜순으로 출력해보긔
 			   - 분류를 기준으로 확인
 			 4. 종료*/
+		
 		Scanner scan = new Scanner(System.in);
 		List<Account> list  = new ArrayList<Account>(); //가계부 
 		Account a ; //건당 지출내역정보
+		char menu = 1;
 		
-		//구현만 내일 메인에서 하면 된다~.~ 
+		do { 
+			printMenu();
+			menu = scan.next().charAt(0);
+			switch(menu) {
+			case '1':
+				//등록
+				System.out.println("추가할 정보를 입력하세요.");
+				a = inputAccount(scan);
+				if(insertAccount(list, a))
+					System.out.println("해당 정보를 가계부에 추가하였습니다.");
+				else 
+					System.out.println("이미 있는 정보이거나 추가에 실패하였습니다.");
+				
+				break;
+			case '2':
+				//수정
+				System.out.println("수정을 원하는 정보를 입력하세요.");
+				a = inputAccount(scan);
+				if(modifyAccount(list, a, scan))
+					System.out.println("해당 정보의 내용을 수정하였습니다.");
+				else
+					System.out.println("! 없는 정보이거나 수정에 실패하였습니다.");
+				break;
+			case '3':
+				//확인
+				printSubMenu();
+				char submenu = scan.next().charAt(0);
+				switch(submenu) {
+				case '1':
+					//일시
+					System.out.println("검색을 원하는 일시를 입력하세요. ");
+					a = inputDate(scan);
+					if(searchDate(list, a))
+						System.out.println("검색을 완료 하였습니다.");
+					else 
+						System.out.println("일치하는 일시가 없거나 검색에 실패했습니다.");
+					break;
+				case '2':
+					//수입&지출
+					// 검색된 수입&지출을 날짜순으로 출력하려는 코드 
+//					System.out.println(list);
+//					Object test[] = (list.toArray());
+//					Arrays.sort(test, new DateDescending());
+//					for(Object tmp : test)
+//						System.out.println((Account)tmp);
+					System.out.println("검색을 원하는 항목을 입력하세요. ");
+					a = inputInAndOut(scan);
+					if(searchInAndOut(list, a))
+						System.out.println("검색을 완료하였습니다.");
+					else 
+						System.out.println("일치하는 항목이 없거나 검색에 실패했습니다.");
+					break;
+				case '3':
+					//분류
+					System.out.println("검색을 원하는 분류명을 입력하세요. ");
+					a = inputCategory(scan);
+					if(searchCategory(list, a))
+						System.out.println("검색을 완료하였습니다.");
+					else 
+						System.out.println("일치하는 분류명이 없거나 검색에 실패했습니다.");
+					break;
+				default:
+					System.out.println("잘못된 입력입니다.");
+				}
+				break;
+			case '4':
+				System.out.println("가계부를 종료합니다.");
+				break;
+			default :
+				System.out.println("잘못된 입력입니다.");
+			}
+		}while(menu!='4');
 		
-		a = inputAccount(scan);
-		insertAccount(list, a);
-
-		a = inputAccount(scan);
-		insertAccount(list, a); 
-		
-		System.out.println(list);
-		
-		a = inputAccount(scan);
-		modifyAccount(list, a, scan);
-		
-		System.out.println(list);
 		
 	}
 
+
+	private static void printMenu() {
+		System.out.println(" ==== 메 뉴 ==== ");
+		System.out.println("   1.가계부 추가");
+		System.out.println("   2.가계부 수정");
+		System.out.println("   3.가계부 확인");
+		System.out.println("   4.종료");
+		System.out.println(" ============= ");
+		System.out.print("메뉴를 선택하세요 : ");
+		
+	}
 	
+	
+	private static void printSubMenu() {
+		// 3.확인의 서브 메뉴 
+		System.out.println(" == 확 인 기 준 == ");
+		System.out.println("   1.일시");
+		System.out.println("   2.수입/지출");
+		System.out.println("   3.분류");
+		System.out.println(" ============= ");
+		System.out.print("메뉴를 선택하세요 : ");
+		
+	}
+	
+
+
 	/*기능 : 스캐너를 통해서 가계부 내용을 입력받아 건당 정보로 반환하는 메서드 
 	 * 매개변수 : List<Account> list , Scanner scan
 	 * 리턴타입 : void
@@ -69,7 +157,7 @@ public class HouseholdAccounts {
 	
 	
 	/*기능 :  입력받은 건당 정보들을 가계부에 저장하는 메서드 , 무사히 저장했으면 true, 이미 있는내용이거나 비어있는 값이어서 저장 못했을 댄 false
-	 * 매개변수 : List<Account> list (가계부) , Account a (하루치정보)
+	 * 매개변수 : List<Account> list (가계부) , Account a (건당 정보)
 	 * 리턴타입 :  boolean
 	 * 메서드명 : insertAccount
 	 * */
@@ -93,7 +181,7 @@ public class HouseholdAccounts {
 		return false;
 		for(int i = 0 ; i<list.size();i++) {
 			if(list.get(i).equals(a)) {
-				System.out.println("변경할 내용을 입력하세요 : ");
+				System.out.print(" > > 변경할 내용을 입력하세요 : ");
 				list.get(i).content = scan.next();
 			}
 		}
@@ -107,7 +195,7 @@ public class HouseholdAccounts {
 	 * */
 	
 	public static Account inputDate(Scanner scan) {
-		System.out.print("검색 일시 : ");
+		System.out.print("일시  예)20-05-21 : ");
 		return new Account(scan.next(),null,null,null,null);
 	}
 	
@@ -138,6 +226,7 @@ public class HouseholdAccounts {
 	}
 	
 	public static boolean searchInAndOut(List<Account> list, Account a ) {
+		
 		int cnt = 0;									
 		for (int i = 0; i < list.size(); i++) {
 			if(list.get(i).inandout.equals(a.inandout)) {
@@ -146,15 +235,15 @@ public class HouseholdAccounts {
 			} 
 		} 
 		if(cnt!=0)
-			return true;
+		return true;
 		else 
 			return false;
 	}
 	
 	
 	public static Account inputCategory(Scanner scan) {
-		System.out.print("분류 검색 : ");
-		return new Account(null,null,scan.next(),null,null);
+		System.out.print("분류명 : ");
+		return new Account(null,null,null,null,scan.next());
 	}
 	
 	public static boolean searchCategory(List<Account> list, Account a ) {
@@ -239,7 +328,7 @@ class Account {
 
 	@Override
 	public String toString() {
-		return  "\n" + date + "\n" + "수입/지출 : " + inandout  +  " 분류 : " + category + " 금액 : " + money + " 내용 : "+content 
+		return  "\n" + date + "\n" + "수입/지출 : " + inandout + "\n" +  "분류 : " + category +  "\n" + "금액 : " + money +  "\n" +"내용 : "+content 
 				 ;
 	}
 	
@@ -254,4 +343,44 @@ class Account {
 		this.category = category;
 	}
 	
+	
+	
 }
+
+
+
+// list.sort로 활용해보긔 
+//class DateDescending implements Comparator{
+//
+//	@Override
+//	public int compare(Object o1, Object o2) {
+//		if(o1 instanceof Account && o2 instanceof Account) {
+//			Account a1 = (Account)o1;
+//			Account a2 = (Account)o2;
+//		int year1 = Integer.parseInt(a1.date.substring(0,2));
+//		int year2 = Integer.parseInt(a2.date.substring(0,2));
+//		int month1 = Integer.parseInt(a1.date.substring(3,5));
+//		int month2 = Integer.parseInt(a2.date.substring(3,5));
+//		int day1 = Integer.parseInt(a1.date.substring(6,8));
+//		int day2 = Integer.parseInt(a2.date.substring(6,8));
+//		if(year1 > year2) 
+//			return 1;
+//		else if (year1 < year2) 
+//			return -1;
+//		
+//		if(month1 > month2) 
+//			return 1;
+//		else if (month1 < month2) 
+//			return -1;
+//		if(day1 > day2) 
+//			return 1;
+//		else if (day1 < day2) 
+//			return -1;
+//		return 0;
+//		}
+//		return 0;
+//	}
+//	
+//}
+
+
